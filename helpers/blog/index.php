51f1f6428376deb94012f155f2b829155817f8ed
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 01:44:50 +0300

    fixed

--- helpers/blog/index.php
+++ helpers/blog/index.php
@@ -1,7 +1,5 @@
 <?php
-// include '../../admin/connection/index.php';
-$connect = $db = mysqli_connect('localhost', 'u976524705_e4c', 'Gudboy24@', 'u976524705_e4c');
-// Fetch blogs from the database
+include 'admin/connection/index.php';
 $query = "SELECT * FROM blogs";
 $result = mysqli_query($connect, $query);
 ?>
