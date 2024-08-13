c5c7060c40a7219eb8e34f3ecc1737313ae27c27
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 01:56:13 +0300

    fixed

--- helpers/blog/index.php
+++ helpers/blog/index.php
@@ -1,8 +1,6 @@
 <?php
-include '../connection/index.php';
-
-// Fetch blogs from the database
-$query = "SELECT * FROM blogs";
+include 'admin/connection/index.php';
+$query = "SELECT * FROM blogs ORDER BY id DESC";
 $result = mysqli_query($connect, $query);
 ?>
 
