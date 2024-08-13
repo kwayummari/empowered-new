916abda1675c46997bec087c7b7fce127506ad43
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 02:19:11 +0300

    fixed

--- helpers/blog/index.php
+++ helpers/blog/index.php
@@ -1,6 +1,6 @@
 <?php
 include 'admin/connection/index.php';
-$query = "SELECT * FROM blogs ORDER BY id DESC";
+$query = "SELECT * FROM blogs ORDER BY id DESC LIMIT 3";
 $result = mysqli_query($connect, $query);
 ?>
 
