43227ea826155297d421fbffddbf6cf1e1146532
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-13 16:21:23 +0300

    fixed

--- helpers/partnership/index.php
+++ helpers/partnership/index.php
@@ -1,19 +1,27 @@
 <div class="donate" data-parallax="scroll" data-image-src="img/topper2.webp">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-7">
-                        <div class="donate-content">
-                            <div class="section-header">
-                                <p>WE BELIEVE IN PARTNERSHIP </p>
-                                <h2>We expect to work with various partners.</h2>
-                            </div>
-                            <div class="donate-text">
-                                <p>
-                                    The organisation will work closely with the government through the ministry of Education and the ministry responsible for youth together with other development partners such as the United Nations System in Tanzania and the private sector to ensure that all opportunities provided for youth in sustainable development goods are being accumulated effectively by them.
-                                </p>
-                            </div>
-                        </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-7">
+                <div class="donate-content">
+                    <div class="section-header">
+                        <p>WE BELIEVE IN PARTNERSHIP </p>
+                        <h2>We expect to work with various partners.</h2>
+                    </div>
+                    <div class="donate-text">
+                        <p>
+                            The organization will work closely with
+                            the government through the ministry of
+                            Community Development, Gender, Women and
+                            Special Groups together with other development
+                            partners such as the United Nations System
+                            in Tanzania and the private sector to ensure
+                            that all opportunities provided for women in
+                            sustainable development goods are being accumulated
+                            effectively by them.
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
