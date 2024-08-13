6900a2504150432e7d676d651d5d3bf6c7c10661
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 01:38:48 +0300

    fixed

--- helpers/blog/index.php
+++ helpers/blog/index.php
@@ -55,28 +55,3 @@ $result = mysqli_query($connect, $query);
         </div>
     </div>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var blogModal = document.getElementById('blogModal');
-
-    blogModal.addEventListener('show.bs.modal', function (event) {
-        var button = event.relatedTarget; // Button that triggered the modal
-        var title = button.getAttribute('data-title');
-        var caption = button.getAttribute('data-caption');
-        var description = button.getAttribute('data-description');
-        var image = button.getAttribute('data-image');
-
-        var modalTitle = blogModal.querySelector('#modalTitle');
-        var modalCaption = blogModal.querySelector('#modalCaption');
-        var modalDescription = blogModal.querySelector('#modalDescription');
-        var modalImage = blogModal.querySelector('#modalImage');
-
-        modalTitle.textContent = title;
-        modalCaption.textContent = caption;
-        modalDescription.textContent = description;
-        modalImage.src = image;
-    });
-});
-</script>
