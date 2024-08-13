73466a4118e9b7295caee38f56134823cdea12d7
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-13 18:11:19 +0300

    fixed

--- admin/header/header.php
+++ admin/header/header.php
@@ -6,7 +6,7 @@ $username = $_SESSION['username'];
 <header id="header" class="header fixed-top d-flex align-items-center">
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
+        <img src="assets/img/logo/logo.png" alt="">
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div>
