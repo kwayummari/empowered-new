191ae099594e34053af26cdc9905e40d1a58f8a0
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-13 16:45:06 +0300

    fixed

--- helpers/team/index.php
+++ helpers/team/index.php
@@ -1,31 +1,3 @@
-<div class="team">
-    <div class="container">
-        <div class="section-header text-center">
-            <p>Meet Our Board Members</p>
-            <h2>Awesome guys behind our activities</h2>
-        </div>
-        <div class="row">
-            <!-- Reusable team member component -->
-            <div class="col-lg-3 col-md-6" v-for="member in teamMembers" :key="member.name">
-                <div class="team-item">
-                    <div class="team-img">
-                        <img :src="member.image" alt="Team Image">
-                    </div>
-                    <div class="team-text">
-                        <h2>{{ member.name }}</h2>
-                        <p>{{ member.role }}</p>
-                        <div class="team-social">
-                            <a v-for="social in member.socialLinks" :key="social.platform" :href="social.link">
-                                <i :class="`fab fa-${social.platform}`"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <script>
     const app = new Vue({
         el: '.team',
@@ -79,3 +51,30 @@
         }
     });
 </script>
+<div class="team">
+    <div class="container">
+        <div class="section-header text-center">
+            <p>Meet Our Board Members</p>
+            <h2>Awesome guys behind our activities</h2>
+        </div>
+        <div class="row">
+            <!-- Reusable team member component -->
+            <div class="col-lg-3 col-md-6" v-for="member in teamMembers" :key="member.name">
+                <div class="team-item">
+                    <div class="team-img">
+                        <img :src="member.image" alt="Team Image">
+                    </div>
+                    <div class="team-text">
+                        <h2>{{ member.name }}</h2>
+                        <p>{{ member.role }}</p>
+                        <div class="team-social">
+                            <a v-for="social in member.socialLinks" :key="social.platform" :href="social.link">
+                                <i :class="`fab fa-${social.platform}`"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
