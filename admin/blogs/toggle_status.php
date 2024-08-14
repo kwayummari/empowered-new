b0f6a7487779da45a987d87a11b7569adf325a03
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 03:39:21 +0300

    fixed

--- admin/blogs/toggle_status.php
+++ admin/blogs/toggle_status.php
@@ -3,18 +3,18 @@ session_start();
 include '../connection/index.php';
 if (isset($_GET['id'])) {
     $productId = intval($_GET['id']);
-    $query = "SELECT status FROM products WHERE id = '$productId'";
+    $query = "SELECT status FROM blogs WHERE id = '$productId'";
     $result = mysqli_query($connect, $query);
     $product = mysqli_fetch_assoc($result);
     if ($product) {
         $newStatus = $product['status'] == 1 ? 0 : 1;
-        $update_query = "UPDATE products SET status = '$newStatus' WHERE id = '$productId'";
+        $update_query = "UPDATE blogs SET status = '$newStatus' WHERE id = '$productId'";
         mysqli_query($connect, $update_query);
         header("Location: index.php");
         exit;
     } else {
-        echo "Product not found.";
+        echo "Blog not found.";
     }
 } else {
-    echo "No product ID provided.";
+    echo "No blog ID provided.";
 }
--- admin/internship/toggle_status.php
+++ admin/internship/toggle_status.php
@@ -3,12 +3,12 @@ session_start();
 include '../connection/index.php';
 if (isset($_GET['id'])) {
     $productId = intval($_GET['id']);
-    $query = "SELECT status FROM products WHERE id = '$productId'";
+    $query = "SELECT status FROM internship WHERE id = '$productId'";
     $result = mysqli_query($connect, $query);
     $product = mysqli_fetch_assoc($result);
     if ($product) {
         $newStatus = $product['status'] == 1 ? 0 : 1;
-        $update_query = "UPDATE products SET status = '$newStatus' WHERE id = '$productId'";
+        $update_query = "UPDATE internship SET status = '$newStatus' WHERE id = '$productId'";
         mysqli_query($connect, $update_query);
         header("Location: index.php");
         exit;
