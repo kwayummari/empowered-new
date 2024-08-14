64e2e97bf7677ba762f9b4f15f5d468bc0a82788
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 04:16:52 +0300

    fixed

--- helpers/navbar/index.php
+++ helpers/navbar/index.php
@@ -1,19 +1,25 @@
 <div class="navbar navbar-expand-lg bg-dark navbar-dark">
-            <div class="container-fluid">
-                <a href="index.php" class="navbar-brand"><img src="img/logo/logo.png" alt=""></a>
-                <h4 style="color: #F27D2D;">EMPOWERED FOR CHANGE</h4>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="container-fluid">
+        <a href="index.php" class="navbar-brand"><img src="img/logo/logo.png" alt=""></a>
+        <h4 style="color: #F27D2D;">EMPOWERED FOR CHANGE</h4>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    <div class="navbar-nav ml-auto">
-                        <a href="index.php" class="nav-item nav-link active">Home</a>
-                        <a href="about.php" class="nav-item nav-link">About</a>
-                        <a href="causes.php" class="nav-item nav-link">Causes</a>
-                        <a href="blogs.php" class="nav-item nav-link">Blogs</a>
-                        <a href="#" class="nav-item nav-link">Events</a>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav ml-auto">
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="about.php" class="nav-item nav-link">About</a>
+                <a href="causes.php" class="nav-item nav-link">Causes</a>
+                <a href="blogs.php" class="nav-item nav-link">Blogs</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Work with us</a>
+                    <div class="dropdown-menu">
+                        <a href="internship.php" class="dropdown-item">Internship</a>
+                        <a href="jobs.php" class="dropdown-item">Job Adverts</a>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
