9ce5f681caf55aeb08dd1703d9ec2d4309033f58
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 04:25:58 +0300

    fixed

--- internship.php
+++ internship.php
@@ -33,8 +33,8 @@ $result = mysqli_query($connect, $query);
     <div class="blog">
         <div class="container">
             <div class="section-header text-center">
-                <p>Our Blog</p>
-                <h2>Latest news & articles directly from our blog</h2>
+                <p>Our Internship</p>
+                <h2>Latest internship post</h2>
             </div>
             <div class="row">
                 <?php while ($internship = mysqli_fetch_assoc($result)) { ?>
