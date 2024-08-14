ef0ff13919a6f5331fd251b0dc2979d77bd45cc5
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 03:48:55 +0300

    fixed

--- admin/jobs/update.php
+++ admin/jobs/update.php
@@ -2,9 +2,9 @@
 session_start();
 include '../connection/index.php';
 $id = $_GET['id'];
-$query = "SELECT * FROM blogs WHERE id = '$id'";
+$query = "SELECT * FROM jobs WHERE id = '$id'";
 $result4 = mysqli_query($connect, $query);
-$blog = mysqli_fetch_assoc($result4);
+$jobs = mysqli_fetch_assoc($result4);
 
 if (isset($_POST['submit'])) {
     $title = $_POST['title'];
@@ -12,7 +12,7 @@ if (isset($_POST['submit'])) {
     $description = $_POST['description'];
     $file = $_FILES["cv"]["name"];
     $path = $_FILES['cv']['tmp_name'];
-    $folder = "../../img/blogs/";
+    $folder = "../../img/jobs/";
 
     if ($file) {
         $final_name = time() . '-' . str_replace(" ", "-", $file);
@@ -22,7 +22,7 @@ if (isset($_POST['submit'])) {
         $imageQuery = "";
     }
 
-    $updateQuery = "UPDATE blogs SET 
+    $updateQuery = "UPDATE jobs SET 
         title='$title', 
         caption='$caption', 
         description='$description' 
@@ -32,11 +32,11 @@ if (isset($_POST['submit'])) {
     $update = mysqli_query($connect, $updateQuery);
 
     if ($update) {
-        echo "Blog Updated Successfully!";
+        echo "Jobs Updated Successfully!";
         header("Location: index.php");
         exit;
     } else {
-        echo "Failed to Update Blog.";
+        echo "Failed to Update Jobs.";
     }
 }
 ?>
@@ -51,38 +51,38 @@ if (isset($_POST['submit'])) {
       <div class="row">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Update Blog Details</h5>
+            <h5 class="card-title">Update Jobs Details</h5>
             <form method="POST" action="" autocomplete="on" enctype="multipart/form-data">
               <div class="row mb-3">
                 <label for="inputTitle" class="col-sm-2 col-form-label">Title</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" name="title" value="<?php echo htmlspecialchars($blog['title']); ?>">
+                  <input type="text" class="form-control" name="title" value="<?php echo htmlspecialchars($jobs['title']); ?>">
                 </div>
               </div>
               <div class="row mb-3">
                 <label for="inputCaption" class="col-sm-2 col-form-label">Caption</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" name="caption" value="<?php echo htmlspecialchars($blog['caption']); ?>">
+                  <input type="text" class="form-control" name="caption" value="<?php echo htmlspecialchars($jobs['caption']); ?>">
                 </div>
               </div>
               <div class="row mb-3">
                 <label for="inputDescription" class="col-sm-2 col-form-label">Description</label>
                 <div class="col-sm-10">
-                  <textarea rows="20" id="editor" class="form-control" name="description"><?php echo htmlspecialchars($blog['description']); ?></textarea>
+                  <textarea rows="20" id="editor" class="form-control" name="description"><?php echo htmlspecialchars($jobs['description']); ?></textarea>
                 </div>
               </div>
               <div class="row mb-3">
                 <label for="inputImage" class="col-sm-2 col-form-label">Main Image</label>
                 <div class="col-sm-10">
                   <input class="form-control" type="file" name="cv" id="file_id">
-                  <?php if ($blog['image']) { ?>
-                    <img src="../../img/blogs/<?php echo htmlspecialchars($blog['image']); ?>" alt="Current Image" style="margin-top: 10px; max-width: 200px;">
+                  <?php if ($jobs['image']) { ?>
+                    <img src="../../img/blogs/<?php echo htmlspecialchars($jobs['image']); ?>" alt="Current Image" style="margin-top: 10px; max-width: 200px;">
                   <?php } ?>
                 </div>
               </div>
               <div class="row mb-3">
                 <div class="col-sm-10 offset-sm-2">
-                  <button type="submit" name="submit" class="btn btn-primary">Update Blog</button>
+                  <button type="submit" name="submit" class="btn btn-primary">Update Jobs</button>
                 </div>
               </div>
             </form>
