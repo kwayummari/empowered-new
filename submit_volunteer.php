2d32b73b8b41d1169bbff2d693aaac14ad78e901
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 05:45:59 +0300

    fixed

--- submit_volunteer.php
+++ submit_volunteer.php
@@ -21,5 +21,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $stmt->close();
     $conn->close();
+} else {
+    $message = "Failed to submit. Please try again later.";
+    echo $message;
 }
 ?>
