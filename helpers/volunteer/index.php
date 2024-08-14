2f99b4be407813aaaa107645cd38cb0984c92a19
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 05:17:30 +0300

    fixed

--- helpers/volunteer/index.php
+++ helpers/volunteer/index.php
@@ -1,29 +1,3 @@
-<?php
-include 'admin/connection/index.php'; // Adjust the path to your database connection file
-
-$message = '';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $reason = $_POST['reason'];
-
-    // Insert data into the database
-    $stmt = $conn->prepare("INSERT INTO volunteer (name, email, reason) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $name, $email, $reason);
-
-    if ($stmt->execute()) {
-        $message = "Message sent successfully!";
-    } else {
-        $message = "Failed to submit. Please try again later.";
-    }
-
-    $stmt->close();
-    $conn->close();
-}
-?>
-
 <div class="volunteer" data-parallax="scroll" data-image-src="img/topper.webp">
     <div class="container">
         <div class="row align-items-center">
