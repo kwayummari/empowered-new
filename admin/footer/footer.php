00dc15e04509ad67fc7b0adcf5ca1b2d11e0b6cb
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-14 03:53:44 +0300

    fixed

--- admin/footer/footer.php
+++ admin/footer/footer.php
@@ -1,8 +1,8 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>PandaDigital</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>EMPOWERED FOR CHANGE</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed and Maintained by <a href="https://www.serengetibytes.com">Serengeti Bytes</a>
+      Designed and Maintained by <a href="https://www.aurorawavelabs.com">AURORAWAVE LABS</a>
     </div>
   </footer>
