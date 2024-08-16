8cb2b31fd7719e613e9f0ee95086c89ec4a5d40f
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-16 14:15:26 +0300

    fixed

--- helpers/topBar/index.php
+++ helpers/topBar/index.php
@@ -1,4 +1,4 @@
-<div class="top-bar d-none d-md-block" style="color: white">
+<div class="top-bar d-none d-md-block" style="background-color: white;">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-8">
