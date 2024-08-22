dc77ce649cd3c9ba8604936cc7156d5754475cec
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-22 14:15:02 +0300

    fixing get terms of use api

--- helpers/coreValues/index.php
+++ helpers/coreValues/index.php
@@ -1,66 +1,65 @@
 <div class="service">
-            <div class="container">
-                <div class="section-header text-center">
-                <p>An idea built the wall of separation between the sexes, and an idea will crumble it to dust</p>
-                    <h2>OUR CORE VALUES?</h2>
+    <div class="container">
+        <div class="section-header text-center">
+            <p>An idea built the wall of separation between the sexes, and an idea will crumble it to dust</p>
+            <h2>OUR CORE VALUES</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-balance-scale"></i> <!-- Equality icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3>Equality</h3>
+                        <p>Our beneficiary-centered approach meets the needs of women and girls.</p>
+                    </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <!-- <i class="flaticon-diet"></i> -->
-                            </div>
-                            <div class="service-text">
-                                <!-- <h3>Equality</h3> -->
-                                <p>Our beneficiary centered approach meets the needs of women and girls.</p>
-                            </div>
-                        </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-hand-holding-heart"></i> <!-- Empowerment icon -->
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <!-- <i class="flaticon-water"></i> -->
-                            </div>
-                            <div class="service-text">
-                                <!-- <h3>Empowerment</h3> -->
-                                <p>Our innovative approaches deliver sustainable impact.
-                                </p>
-                            </div>
-                        </div>
+                    <div class="service-text">
+                        <h3>Empowerment</h3>
+                        <p>Our innovative approaches deliver sustainable impact.</p>
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <!-- <i class="flaticon-healthcare"></i> -->
-                            </div>
-                            <div class="service-text">
-                                <!-- <h3>Inclusivity</h3> -->
-                                <p>Through learning we deliver quality programs and services.</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-users"></i> <!-- Inclusivity icon -->
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <!-- <i class="flaticon-education"></i> -->
-                            </div>
-                            <div class="service-text">
-                                <!-- <h3>Respect</h3> -->
-                                <p>We achieve results with integrity.</p>
-                            </div>
-                        </div>
+                    <div class="service-text">
+                        <h3>Inclusivity</h3>
+                        <p>Through learning, we deliver quality programs and services.</p>
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <!-- <i class="flaticon-home"></i> -->
-                            </div>
-                            <div class="service-text">
-                                <!-- <h3>Accountability</h3> -->
-                                <p>We build highly motivated teams to deliver excellence .</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-handshake"></i> <!-- Respect icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3>Respect</h3>
+                        <p>We achieve results with integrity.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-check-circle"></i> <!-- Accountability icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3>Accountability</h3>
+                        <p>We build highly motivated teams to deliver excellence.</p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
