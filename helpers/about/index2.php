63b00975ba2ab46bda72962f1e6e42c9c30297a1
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-22 14:07:11 +0300

    fixing get terms of use api

--- helpers/about/index2.php
+++ helpers/about/index2.php
@@ -13,7 +13,7 @@
                     <ul>
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
-                                <i class="fas fa-bullseye" style="font-size: 50px;"></i> Mission
+                                <i class="fas fa-bullseye" style="font-size: 40px;"></i> Mission
                             </a>
                             <p>To empower women, girls and marginalized groups with correct
                             knowledge and skills while creating an enabling environment
@@ -21,7 +21,7 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px;">
-                                <i class="fas fa-eye" style="font-size: 50px;"></i> Vision
+                                <i class="fas fa-eye" style="font-size: 40px;"></i> Vision
                             </a>
                             <p>Empowered women and girls enjoy their rights and fulfill their potentials.</p>
                         </li>
