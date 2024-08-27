939874ca4695ff08e97dc3d311945ed6177ad95b
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-27 16:11:19 +0300

    fixing galleru part

--- helpers/about/index2.php
+++ helpers/about/index2.php
@@ -28,7 +28,7 @@
                         <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
                                 <i class="fas fa-bullseye" style="font-size: 40px;"></i> Mission
                             </a></center>
-                        <p style="text-align: center; font-size: 20px;">To empower women, girls and marginalized groups with correct
+                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">To empower women, girls and marginalized groups with correct
                             knowledge and skills while creating an enabling environment
                             for them to exercise their rights and fulfill their potentials.</p>
                     </div>
@@ -37,7 +37,7 @@
                         <center><a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px;">
                                 <i class="fas fa-eye" style="font-size: 40px;"></i> Vision
                             </a></center>
-                        <p style="text-align: center; font-size: 20px;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
+                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
                     </div>
                 </div>
             </div>
