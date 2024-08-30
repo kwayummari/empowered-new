d1f97c9bf0da7d9f29b206ea2f677370aeda31ab
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-31 02:23:19 +0300

    fixing galleru part

--- helpers/donate/index.php
+++ helpers/donate/index.php
@@ -3,12 +3,12 @@
 </div>
 <div class="d-flex justify-content-center align-items-center bg-light">
     <div class="card p-4" style="max-width: 400px; width: 100%;">
-        <ul class="nav nav-tabs" id="donationTabs" role="tablist">
-            <li class="nav-item"  style="max-width: 100%; white-space: nowrap;" role="presentation">
-                <button class="nav-link active" id="one-time-tab" data-bs-toggle="tab" data-bs-target="#one-time" type="button" role="tab" aria-controls="one-time" aria-selected="true">One Time</button>
+        <ul class="nav nav-tabs w-100" id="donationTabs" role="tablist">
+            <li class="nav-item flex-fill" role="presentation">
+                <button class="nav-link active w-100" id="one-time-tab" data-bs-toggle="tab" data-bs-target="#one-time" type="button" role="tab" aria-controls="one-time" aria-selected="true">One Time</button>
             </li>
-            <li class="nav-item"  style="max-width: 100%; white-space: nowrap;" role="presentation">
-                <button class="nav-link" id="monthly-tab" data-bs-toggle="tab" data-bs-target="#monthly" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
+            <li class="nav-item flex-fill" role="presentation">
+                <button class="nav-link w-100" id="monthly-tab" data-bs-toggle="tab" data-bs-target="#monthly" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
             </li>
         </ul>
         <div class="tab-content mt-3" id="donationTabsContent">
