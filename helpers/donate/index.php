60fcb1a9676d6ef80821aeed3c9979bc766f0f63
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-31 02:15:10 +0300

    fixing galleru part

--- helpers/donate/index.php
+++ helpers/donate/index.php
@@ -1,31 +1,33 @@
-<div class="card p-4" style="max-width: 400px; width: 100%;">
-    <ul class="nav nav-tabs" id="donationTabs" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="one-time-tab" data-bs-toggle="tab" data-bs-target="#one-time" type="button" role="tab" aria-controls="one-time" aria-selected="true">One Time</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="monthly-tab" data-bs-toggle="tab" data-bs-target="#monthly" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
-        </li>
-    </ul>
-    <div class="tab-content mt-3" id="donationTabsContent">
-        <div class="tab-pane fade show active" id="one-time" role="tabpanel" aria-labelledby="one-time-tab">
-            <div class="d-flex overflow-auto" style="max-width: 100%; white-space: nowrap;">
-                <button type="button" class="btn btn-outline-primary me-2">$10</button>
-                <button type="button" class="btn btn-outline-primary me-2">$25</button>
-                <button type="button" class="btn btn-outline-primary me-2">$50</button>
-                <button type="button" class="btn btn-outline-primary me-2">$100</button>
-                <button type="button" class="btn btn-outline-primary me-2">$250</button>
+<div class="d-flex justify-content-center align-items-center bg-light" style="height: 100vh;">
+    <div class="card p-4" style="max-width: 400px; width: 100%;">
+        <ul class="nav nav-tabs" id="donationTabs" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="one-time-tab" data-bs-toggle="tab" data-bs-target="#one-time" type="button" role="tab" aria-controls="one-time" aria-selected="true">One Time</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="monthly-tab" data-bs-toggle="tab" data-bs-target="#monthly" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
+            </li>
+        </ul>
+        <div class="tab-content mt-3" id="donationTabsContent">
+            <div class="tab-pane fade show active" id="one-time" role="tabpanel" aria-labelledby="one-time-tab">
+                <div class="d-flex overflow-auto" style="max-width: 100%; white-space: nowrap;">
+                    <button type="button" class="btn btn-outline-primary me-2">$10</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$25</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$50</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$100</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$250</button>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="monthly" role="tabpanel" aria-labelledby="monthly-tab">
+                <div class="d-flex overflow-auto" style="max-width: 100%; white-space: nowrap;">
+                    <button type="button" class="btn btn-outline-primary me-2">$5/month</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$10/month</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$20/month</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$50/month</button>
+                    <button type="button" class="btn btn-outline-primary me-2">$100/month</button>
+                </div>
             </div>
         </div>
-        <div class="tab-pane fade" id="monthly" role="tabpanel" aria-labelledby="monthly-tab">
-            <div class="d-flex overflow-auto" style="max-width: 100%; white-space: nowrap;">
-                <button type="button" class="btn btn-outline-primary me-2">$5/month</button>
-                <button type="button" class="btn btn-outline-primary me-2">$10/month</button>
-                <button type="button" class="btn btn-outline-primary me-2">$20/month</button>
-                <button type="button" class="btn btn-outline-primary me-2">$50/month</button>
-                <button type="button" class="btn btn-outline-primary me-2">$100/month</button>
-            </div>
-        </div>
+        <button type="button" class="btn btn-primary w-100 mt-3">Donate Now</button>
     </div>
-    <button type="button" class="btn btn-primary w-100 mt-3">Donate Now</button>
 </div>
