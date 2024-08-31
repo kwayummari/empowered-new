6523a2de6d4da19a848d3ddebaa3ae563c2cec5b
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-31 11:50:53 +0300

    fixing galleru part

--- helpers/about/index3.php
+++ helpers/about/index3.php
@@ -19,6 +19,22 @@
                             reproductive health goals by providing them with tools to make informed 
                             decisions and creating enabling environment for them to fulfill their potentials.  </p>
                     </div>
+                    <br><br>
+                    <div class="col-lg-12">
+                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
+                                <i class="fas fa-bullseye" style="font-size: 40px;"></i> Mission
+                            </a></center>
+                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">To empower women, girls and marginalized groups with correct
+                            knowledge and skills while creating an enabling environment
+                            for them to exercise their rights and fulfill their potentials.</p>
+                    </div>
+                    <br><br>
+                    <div class="col-lg-12">
+                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px;">
+                                <i class="fas fa-eye" style="font-size: 40px;"></i> Vision
+                            </a></center>
+                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
+                    </div>
                 </div>
             </div>
         </div>
