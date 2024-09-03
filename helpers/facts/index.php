a90add3a459b33e59e48ef21e088ec7c1a6b75e2
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-03 04:04:16 +0300

    fixing galleru part

--- helpers/facts/index.php
+++ helpers/facts/index.php
@@ -19,13 +19,13 @@
                             <i class="flaticon-kindness"></i>
                             <div class="facts-text">
                                 <h3 class="facts-plus" data-toggle="counter-up">500</h3>
-                                <p>Women Affected</p>
+                                <p>Women Reached</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="facts-item">
-                            <i class="flaticon-charity"></i>
+                            <img src="img/icons/1.jpg"/>
                             <div class="facts-text">
                                 <h3 class="facts-percentage" data-toggle="counter-up">12</h3>
                                 <p>Increased SRH/FP budget allocation</p>
@@ -34,7 +34,7 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="facts-item">
-                            <i class="flaticon-home"></i>
+                        <img src="img/icons/2.webp"/>
                             <div class="facts-text">
                                 <h3 class="facts-percentage" data-toggle="counter-up">17</h3>
                                 <p>Increased budget allocation for people with disability</p>
