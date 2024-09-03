1f7bd777a8acd4b1c3979f6bb98b4eee174cab6d
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-03 16:11:32 +0300

    fixing galleru part

--- helpers/partnership/index.php
+++ helpers/partnership/index.php
@@ -1,7 +1,7 @@
 <div class="section-header text-center">
-            <h2>Our Partners</h2>
-        </div>
-<div class="donate" data-parallax="scroll" >
+    <h2>Our Partners</h2>
+</div>
+<div class="donate" data-parallax="scroll" data-image-src="img/topper2.webp">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-7">
@@ -10,12 +10,12 @@
                         <p>WE BELIEVE IN PARTNERSHIP </p>
                         <!-- <h2>We expect to work with various partners.</h2> -->
                     </div>
-                    <div class="donate-text">
-                        <div class="logo-list">
-                            <img src="img/partners/1.png" alt="Logo 1">
-                            <img src="img/partners/2.png" alt="Logo 2">
-                        </div>
-                    </div>
+                </div>
+            </div>
+            <div class="donate-text col-lg-12" style="background-color: white;">
+                <div class="logo-list">
+                    <img src="img/partners/1.png" alt="Logo 1">
+                    <img src="img/partners/2.png" alt="Logo 2">
                 </div>
             </div>
         </div>
