c86392f431a75f75a98b75756d1eb3861ad933c8
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-03 16:37:10 +0300

    fixing galleru part

--- helpers/whatWeDo/index.php
+++ helpers/whatWeDo/index.php
@@ -1,118 +1,134 @@
-<!-- Single Post Start-->
-<div class="single">
+<div class="service">
     <div class="container">
+        <div class="section-header text-center">
+            <h2>Our Technical Approach</h2>
+        </div>
         <div class="row">
-            <div class="col-lg-12">
-                <div class="single-content">
-                    <div class="section-header text-center">
-                        <h2>Our Technical Approach</h2>
+            <!-- Equality -->
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-balance-scale"></i> <!-- Equality icon -->
                     </div>
-
-                    <!-- Paragraph 1 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Training and mentorship: </span>
-                        We have designed simplified training materials grounded on adult learning theories and we use age-appropriate methodologies to make learning more joyful and enjoyable for young people.
-                        <span id="dots1">...</span><span id="more1" style="display:none;">
-                            Our training packages incorporate on-going mentorship to ensure that gained knowledge is sustained and skills are put into practice and become part of day-to-day life of women and girls.
-                        </span>
-                        <a onclick="toggleReadMore('1')" id="readMoreBtn1" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
-                    <!-- Paragraph 2 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Structured dialogues: </span>
-                        Our learning approaches are grounded on theories from various scholars that, learning cannot occur without dialogue and reflection. Reading and listening are not enough.
-                        <span id="dots2">...</span><span id="more2" style="display:none;">
-                            Interaction is required. We believe that, the process of putting ideas into words is itself an important form of active engagement with the content, and a way of growing knowledge (even without feedback). If you have never expressed something in your own words, you do not know it fully. Dialogue is a great way for women and girls to reveal themselves in the words they use and the way those words are delivered. Dialogues spark existing conflicts which would otherwise remain obscured. The conflict can be external—character vs. character or character vs. environment—or the dialogue can reflect the viewpoint character’s internal struggle. We have used these theories to develop a dialogue guide which stimulate self-expression and interactions among peers. By fostering on-going dialogue and participation, women, girls or their influencers can cultivate a deeper understanding of local norms and beliefs, thus laying the groundwork for sustainable behavior change.
-                        </span>
-                        <a onclick="toggleReadMore('2')" id="readMoreBtn2" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
-                    <!-- Paragraph 3 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Domestic resource mobilization to improve quality of SRH services: </span>
-                        Our organization strongly believe that domestic government spending on family planning should become the mainstay of a country’s family planning program, providing a budgeted flow of funds for services and staff.
-                        <span id="dots3">...</span><span id="more3" style="display:none;">
-                            Our advocacy efforts have therefore focused on working with regional, districts and health facilities to increase allocations, disbursement and utilization of resources for family planning, adolescents’ friendly SRH services and SRH services for people with disabilities.
-                        </span>
-                        <a onclick="toggleReadMore('3')" id="readMoreBtn3" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
-                    <!-- Paragraph 4 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Strengthening movement of women and girls led CSOs:</span>
-                        Our organization believes that women and girls are experts of their own needs and therefore meaningful engagement and involvement of women in policy development, planning, budgeting and all SRH interventions is paramount.
-                        <span id="dots4">...</span><span id="more4" style="display:none;">
-                            Our interventions are focused on building the capacity of women and girls led civil society organizations (CSOs) to lead and fully participate in SRH policy making and programming. We strengthen the ability of CSOs to advocate for quality and equitable SRH information and services and hold the government accountable on its SRH commitments.
-                        </span>
-                        <a onclick="toggleReadMore('4')" id="readMoreBtn4" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
-                    <div class="section-header text-center">
-                        <h2>Our Core Strength</h2>
+                    <div class="service-text">
+                        <h3><a href="#" data-toggle="modal" data-target="#equalityModal">Training and mentorship:</a></h3>
+                        <p>We have designed simplified training materials grounded on adult learning theories and we use age-appropriate methodologies ...</p>
+                        <a href="#" data-toggle="modal" data-target="#equalityModal">Read More</a>
                     </div>
+                </div>
+            </div>
 
-                    <!-- Paragraph 5 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Innovative programming: </span>
-                        Our interventions always seek creation and implementation of new and creative methods and techniques which will enhance our efficiency and effectiveness in reaching more women and girls.
-                        <span id="dots5">...</span><span id="more5" style="display:none;">
-                            We constantly seek feedback from our beneficiaries to learn what is working and what is not, and redesign our approaches to ensure that more women and girls are reached. We constantly explore new technology and take advantage of technological advancement to improve both reach and the impact of our interventions.
-                        </span>
-                        <a onclick="toggleReadMore('5')" id="readMoreBtn5" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
+            <!-- Empowerment -->
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-hand-holding-heart"></i> <!-- Empowerment icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3><a href="#" data-toggle="modal" data-target="#empowermentModal">Structured dialogues:</a></h3>
+                        <p>Our innovative approaches are designed to create lasting and meaningful change. By leveraging new ideas and cutting-edge solutions, we address challenges in unique and effective ways...</p>
+                        <a href="#" data-toggle="modal" data-target="#empowermentModal">Read More</a>
+                    </div>
+                </div>
+            </div>
 
-                    <!-- Paragraph 6 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Focus on depth and not just scale: </span>
-                        In our organization, prioritizing depth over mere target achievement is paramount. Rather than focusing solely on hitting numerical goals, deliberate efforts are made in the design and implementation of the program towards fostering genuine understanding and empathy among women and girls.
-                        <span id="dots6">...</span><span id="more6" style="display:none;">
-                            This entails tailoring interventions to address the multifaceted layers of social and gender norms, integrating culturally sensitive approaches that resonate with local contexts. Through this approach, the seeds of transformation can take root, paving the way for meaningful shifts in attitudes and behaviors among women, girls and their influencers.
-                        </span>
-                        <a onclick="toggleReadMore('6')" id="readMoreBtn6" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
+            <!-- Inclusivity -->
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-users"></i> <!-- Inclusivity icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3><a href="#" data-toggle="modal" data-target="#inclusivityModal">Domestic resource mobilization to improve quality of SRH services:</a></h3>
+                        <p>Our organization strongly believe that domestic government spending on family planning should become the mainstay of a country’s family planning program, providing a budgeted flow of funds for services and staff...</p>
+                        <a href="#" data-toggle="modal" data-target="#inclusivityModal">Read More</a>
+                    </div>
+                </div>
+            </div>
 
-                    <!-- Paragraph 7 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Gender Equality and Social Inclusion mainstreaming: </span>
-                        Gender Equality and Social Inclusion (GESI) is a cornerstone of our programming. Our programme activities are designed with both practical and strategic needs of women and girls, and other excluded groups in mind.
-                        <span id="dots7">...</span><span id="more7" style="display:none;">
-                            The interventions, therefore, aim at shifting social norms and power dynamics to ensure women and girls and other excluded groups can enjoy their rights, and have equal access to and control over assets, resources and services.
-                        </span>
-                        <a onclick="toggleReadMore('7')" id="readMoreBtn7" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
-                    <!-- Paragraph 8 with Read More -->
-                    <p style="margin-bottom: 60px; font-size: 20px;">
-                        <span style="color: #F27D2D; font-weight: bold; font-style: italic;">Institutionalized intersectionality: </span>
-                        Our interventions always seek to understand the ways that multiple forms of inequality or disadvantage sometimes compound themselves and create obstacles that often are not understood among conventional ways of thinking.
-                        <span id="dots8">...</span><span id="more8" style="display:none;">
-                            The design of our program strongly considers how groups' and individuals' social and political identities result in unique combinations of discrimination and privilege. Examples of these factors include gender, caste, sex, race, ethnicity, class, sexuality, religion, disability, height, age, weight and physical appearance. We therefore design programs which cut across multiple social and political entities of an individual and empower them to improve various aspects of their lives.
-                        </span>
-                        <a onclick="toggleReadMore('8')" id="readMoreBtn8" style="color: #F27D2D; cursor: pointer;">Read More</a>
-                    </p>
-
+            <!-- Respect -->
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <div class="service-icon">
+                        <i class="fas fa-handshake"></i> <!-- Respect icon -->
+                    </div>
+                    <div class="service-text">
+                        <h3><a href="#" data-toggle="modal" data-target="#respectModal">Strengthening movement of women and girls led CSOs:</a></h3>
+                        <p>Our organization believes that women and girls are experts of their own needs and therefore meaningful engagement and involvement of women in policy development, planning, budgeting and all SRH interventions is paramount. ...</p>
+                        <a href="#" data-toggle="modal" data-target="#respectModal">Read More</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- Single Post End-->
 
-<script>
-    function toggleReadMore(id) {
-        var dots = document.getElementById("dots" + id);
-        var moreText = document.getElementById("more" + id);
-        var btnText = document.getElementById("readMoreBtn" + id);
+<!-- Equality Modal -->
+<div class="modal fade" id="equalityModal" tabindex="-1" role="dialog" aria-labelledby="equalityModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="equalityModalLabel">Training and mentorship:</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>We have designed simplified training materials grounded on adult learning theories and we use age-appropriate methodologies to make learning more joyful and enjoyable for young people. Our training packages incorporate on-going mentorship to ensure that gained knowledge is sustained and skills are put into practice and become part of day-to-day life of women and girls.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-        if (dots.style.display === "none") {
-            dots.style.display = "inline";
-            btnText.innerHTML = "Read More";
-            moreText.style.display = "none";
-        } else {
-            dots.style.display = "none";
-            btnText.innerHTML = "Read Less";
-            moreText.style.display = "inline";
-        }
-    }
-</script>
+<!-- Empowerment Modal -->
+<div class="modal fade" id="empowermentModal" tabindex="-1" role="dialog" aria-labelledby="empowermentModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="empowermentModalLabel">Structured dialogues</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Our learning approaches are grounded on theories from various scholars that, learning cannot occur without dialogue and reflection. Reading and listening are not enough. Interaction is required. We believe that, the process of putting ideas into words is itself an important form of active engagement with the content, and a way of growing knowledge (even without feedback). If you have never expressed something in your own words, you do not know it fully. Dialogue is a great way for women and girls to reveal themselves in the words they use and the way those words are delivered. Dialogues spark existing conflicts which would otherwise remain obscured. The conflict can be external—character vs. character or character vs. environment—or the dialogue can reflect the viewpoint character’s internal struggle. We have used these theories to develop a dialogue guide which stimulate self-expression and interactions among peers. By fostering on-going dialogue and participation, women, girls or their influencers can cultivate a deeper understanding of local norms and beliefs, thus laying the groundwork for sustainable behavior change.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Inclusivity Modal -->
+<div class="modal fade" id="inclusivityModal" tabindex="-1" role="dialog" aria-labelledby="inclusivityModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="inclusivityModalLabel">Domestic resource mobilization to improve quality of SRH services:</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Our organization strongly believe that domestic government spending on family planning should become the mainstay of a country’s family planning program, providing a budgeted flow of funds for services and staff. Our advocacy efforts have therefore focused on working with regional, districts and health facilities to increase allocations, disbursement and utilization of resources for family planning, adolescents’ friendly SRH services and SRH services for people with disabilities.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Respect Modal -->
+<div class="modal fade" id="respectModal" tabindex="-1" role="dialog" aria-labelledby="respectModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="respectModalLabel">Strengthening movement of women and girls led CSOs</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Our organization believes that women and girls are experts of their own needs and therefore meaningful engagement and involvement of women in policy development, planning, budgeting and all SRH interventions is paramount. Our interventions are focused on building the capacity of women and girls led civil society organizations (CSOs) to lead and fully participate in SRH policy making and programming. We strengthen the ability of CSOs to advocate for quality and equitable SRH information and services and hold the government accountable on its SRH commitments.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
