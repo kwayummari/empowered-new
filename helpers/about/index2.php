e27aea23c0d209ed56daf7a73c3caaef3d304ccd
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 15:27:57 +0300

    small fixes

--- helpers/about/index2.php
+++ helpers/about/index2.php
@@ -1,19 +1,9 @@
 <div class="about">
     <div class="container">
         <div class="row align-items-center">
-            <!-- <div class="col-lg-6">
-                <div class="about-img" data-parallax="scroll" data-image-src="img/about/about.webp"></div>
-            </div> -->
             <div class="col-lg-12">
-                <div class="section-header">
-                    <!-- <p>Learn About Us</p>
-                    <h2>EMPOWERED FOR CHANGE</h2> -->
-                </div>
                 <div>
                     <div class="col-lg-12">
-                        <!-- <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
-                                <i class="fas fa-book" style="font-size: 40px;"></i> About
-                            </a></center> -->
                         <p style="text-align: center; font-size: 20px; color: #20212b;">We are a women's rights organization 
                             that seeks to empower women and girls of all abilities to realize their 
                             reproductive health goals by providing them with tools to make informed 
