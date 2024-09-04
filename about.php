4035ce8adda7e86ff66c424233bffda2f4d89216
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 14:37:32 +0300

    fixing galleru part

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <a href="index.php">Home </a>
+                    <a href="index.php">Home</a>
                     <a href="#">Who we are</a>
                 </div>
             </div>
--- causes.php
+++ causes.php
@@ -13,7 +13,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <a href="index.php">Home </a>
+                        <a href="index.php">Home</a>
                         <a href="#">What we do</a>
                     </div>
                 </div>
