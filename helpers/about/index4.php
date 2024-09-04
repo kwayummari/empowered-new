8e163a8a6452fce47ad250a0a421aaf34950a8a3
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 15:24:06 +0300

    small fixes

--- helpers/about/index4.php
+++ helpers/about/index4.php
@@ -5,18 +5,18 @@
                 <div>
                     <div class="col-lg-12">
                         <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
-                                <i class="fas fa-bullseye" style="font-size: 40px;"></i> Mission
+                                <i class="fas fa-bullseye" style="font-size: 40px; color: #F27D2D"></i> Mission
                             </a></center>
-                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">To empower women, girls and marginalized groups with correct
+                        <p style="text-align: center; font-size: 20px; color: #4a4c70;">To empower women, girls and marginalized groups with correct
                             knowledge and skills while creating an enabling environment
                             for them to exercise their rights and fulfill their potentials.</p>
                     </div>
                     <br><br>
                     <div class="col-lg-12">
                         <center><a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px;">
-                                <i class="fas fa-eye" style="font-size: 40px;"></i> Vision
+                                <i class="fas fa-eye" style="font-size: 40px; color: #F27D2D"></i> Vision
                             </a></center>
-                        <p style="text-align: center; font-size: 20px; color: #F27D2D;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
+                        <p style="text-align: center; font-size: 20px; color: #4a4c70;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
                     </div>
                 </div>
             </div>
