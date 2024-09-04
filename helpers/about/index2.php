33593813f60aeaf18a312069b3f8dd9e61536d6d
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 19:14:34 +0300

    fixing galleru part

--- helpers/about/index2.php
+++ helpers/about/index2.php
@@ -4,7 +4,7 @@
             <div class="col-lg-12">
                 <div>
                     <div class="col-lg-12">
-                        <p style="text-align: center; font-size: 40px; color: #20212b;">We are a women's rights organization 
+                        <p style="text-align: center; font-size: 30px; color: #20212b;">We are a women's rights organization 
                             that seeks to empower women and girls of all abilities to realize their 
                             reproductive health goals by providing them with tools to make informed 
                             decisions and creating enabling environment for them to fulfill their potentials.  </p>
