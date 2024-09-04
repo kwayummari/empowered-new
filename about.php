033fc6abf7416a1a49a22d7beecc0f2db95ab9d0
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 14:29:48 +0300

    fixing galleru part

--- about.php
+++ about.php
@@ -14,10 +14,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <!-- <h2>About Us</h2> -->
-                </div>
-                <div class="col-12">
-                    <a href="index.php">Home /</a>
+                    <a href="index.php">Home </a>
                     <a href="#">Who we are</a>
                 </div>
             </div>
