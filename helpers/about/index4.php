51d45ac15ff1739892ecc74f1054299d47f6eb52
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-04 15:25:21 +0300

    small fixes

--- helpers/about/index4.php
+++ helpers/about/index4.php
@@ -4,7 +4,7 @@
             <div class="col-lg-12">
                 <div>
                     <div class="col-lg-12">
-                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px;">
+                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-2" style="font-size: 30px; color: #F27D2D">
                                 <i class="fas fa-bullseye" style="font-size: 40px; color: #F27D2D"></i> Mission
                             </a></center>
                         <p style="text-align: center; font-size: 20px; color: #4a4c70;">To empower women, girls and marginalized groups with correct
@@ -13,7 +13,7 @@
                     </div>
                     <br><br>
                     <div class="col-lg-12">
-                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px;">
+                        <center><a class="nav-link" data-toggle="pill" href="#tab-content-3" style="font-size: 30px; color: #F27D2D">
                                 <i class="fas fa-eye" style="font-size: 40px; color: #F27D2D"></i> Vision
                             </a></center>
                         <p style="text-align: center; font-size: 20px; color: #4a4c70;">Empowered women and girls enjoy their rights and fulfill their potentials.</p>
